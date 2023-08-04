74d010459a05791db60c2b8fe0c64a621481f50e
baxtian7 <sebastiangomezjimenez8@gmail.com>
2023-08-04 16:34:41 -0500

    feat: :zap: Seeder is added to add admin options in menu

--- database/seeders/UsersSeeder.php
+++ database/seeders/UsersSeeder.php
@@ -40,6 +40,37 @@ class UsersSeeder extends Seeder
                 'updated_at' => '2022-09-13 17:29:44',
                 'created_at' => '2023-07-10 17:29:44',
             ),
+            1 =>
+            array(
+                'name' => 'role.index',
+                'description' => 'show',
+                'url' => '/admin/role',
+                'guard_name' => 'role',
+                'slug' => 'role',
+                'updated_at' => '2022-09-13 17:29:44',
+                'created_at' => '2023-07-10 17:29:44',
+            ),
+            2 =>
+            array(
+                'name' => 'permissions.index',
+                'description' => 'show',
+                'url' => '/admin/permissions',
+                'guard_name' => 'permissions',
+                'slug' => 'permissions',
+                'updated_at' => '2022-09-13 17:29:44',
+                'created_at' => '2023-07-10 17:29:44',
+            ),
+            3 =>
+            array(
+                'name' => 'user.index',
+                'description' => 'show',
+                'url' => '/admin/user',
+                'guard_name' => 'user',
+                'slug' => 'user',
+                'updated_at' => '2022-09-13 17:29:44',
+                'created_at' => '2023-07-10 17:29:44',
+            ),
+
         ));
 
         DB::table('roles')->insert(array(
@@ -58,6 +89,21 @@ class UsersSeeder extends Seeder
                 'permission_id' => 1,
                 'role_id' => 1
             ),
+            1 =>
+            array(
+                'permission_id' => 2,
+                'role_id' => 1
+            ),
+            2 =>
+            array(
+                'permission_id' => 3,
+                'role_id' => 1
+            ),
+            3 =>
+            array(
+                'permission_id' => 4,
+                'role_id' => 1
+            ),
         ));
 
         
@@ -70,5 +116,49 @@ class UsersSeeder extends Seeder
 
             ),
         ));
+
+        DB::table('menu_items')->insert(array(
+            0 =>
+            array(
+                'name'                  =>'menu',
+                'link'                  =>'/admin/menu-item',
+                'icon'                  =>'fas fa-align-center',
+                'order'                 =>'0',
+                'created_at'            =>'2023-08-04 20:19:55',
+                'updated_at'            =>'2023-08-04 20:19:55',
+                'permission_id'         =>1,
+            ),
+            1 =>
+            array(
+                'name'                  =>'rol',
+                'link'                  =>'/admin/role',
+                'icon'                  =>'fas fa-align-center',
+                'order'                 =>'0',
+                'created_at'            =>'2023-08-04 20:19:55',
+                'updated_at'            =>'2023-08-04 20:19:55',
+                'permission_id'         =>1,
+            ),
+            2 =>
+            array(
+                'name'                  =>'permisos',
+                'link'                  =>'/admin/permissions',
+                'icon'                  =>'fas fa-align-center',
+                'order'                 =>'0',
+                'created_at'            =>'2023-08-04 20:19:55',
+                'updated_at'            =>'2023-08-04 20:19:55',
+                'permission_id'         =>1,
+            ),
+            3 =>
+            array(
+                'name'                  =>'user',
+                'link'                  =>'/admin/user',
+                'icon'                  =>'fas fa-align-center',
+                'order'                 =>'0',
+                'created_at'            =>'2023-08-04 20:19:55',
+                'updated_at'            =>'2023-08-04 20:19:55',
+                'permission_id'         =>1,
+            ),
+        ));
+
     }
 }
