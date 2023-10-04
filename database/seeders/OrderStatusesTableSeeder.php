31be6ffebb7d31ef67210c3c2498eb8ff7d9e20f
Yonnys Quintero <leafar.qr@gmail.com>
2023-10-04 10:26:15 -0400

    seeder order statuses

--- database/seeders/OrderStatusesTableSeeder.php
+++ database/seeders/OrderStatusesTableSeeder.php
@@ -21,48 +21,48 @@ class OrderStatusesTableSeeder extends Seeder
         \DB::table('order_statuses')->insert(array (
             0 => 
             array (
-                'color' => '#3B82F6',
-                'created_at' => '2023-08-29 19:31:20',
-                'description' => 'Estado inicial del proceso',
                 'id' => 1,
                 'name' => 'En espera',
-                'updated_at' => '2023-08-29 19:31:20',
+                'description' => 'Estado inicial del proceso',
+                'color' => '#ffe6c0',
+                'created_at' => '2023-08-29 19:31:20',
+                'updated_at' => '2023-09-27 16:54:56',
             ),
             1 => 
             array (
-                'color' => '#FFE6C0',
-                'created_at' => '2023-08-29 19:31:20',
-                'description' => 'Estado posterior a la aprobacion de la orden',
                 'id' => 2,
                 'name' => 'En pickeo',
-                'updated_at' => '2023-08-29 19:31:20',
+                'description' => 'Estado posterior a la aprobacion de la orden',
+                'color' => '#a9b7fe',
+                'created_at' => '2023-08-29 19:31:20',
+                'updated_at' => '2023-10-03 00:36:47',
             ),
             2 => 
             array (
-                'color' => '#FFE6C0',
-                'created_at' => '2023-08-29 19:31:20',
-                'description' => 'Estado posterior a la aprobacion de la orden por parte del picker',
                 'id' => 3,
                 'name' => 'En revisión',
-                'updated_at' => '2023-08-29 19:31:20',
+                'description' => 'Estado posterior a la aprobacion de la orden por parte del picker',
+                'color' => '#d5ebf8',
+                'created_at' => '2023-08-29 19:31:20',
+                'updated_at' => '2023-09-27 16:56:56',
             ),
             3 => 
             array (
-                'color' => '#FFE6C0',
-                'created_at' => '2023-08-29 19:31:20',
-                'description' => 'Regresa a revision por CDA dado algun problema',
                 'id' => 4,
                 'name' => 'Rechazada',
-                'updated_at' => '2023-08-29 19:31:20',
+                'description' => 'Regresa a revision por CDA dado algun problema',
+                'color' => '#ff9494',
+                'created_at' => '2023-08-29 19:31:20',
+                'updated_at' => '2023-09-27 16:58:14',
             ),
             4 => 
             array (
-                'color' => '#FFE6C0',
-                'created_at' => '2023-08-29 19:31:20',
-                'description' => 'Aprovado por picker y revisor',
                 'id' => 5,
                 'name' => 'Revisado',
-                'updated_at' => '2023-08-29 19:31:20',
+                'description' => 'Aprovado por picker y revisor',
+                'color' => '#d4ffc0',
+                'created_at' => '2023-08-29 19:31:20',
+                'updated_at' => '2023-09-27 16:56:30',
             ),
         ));
         
