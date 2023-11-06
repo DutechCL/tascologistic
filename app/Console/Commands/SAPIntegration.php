764a4b7c4fe64a2984f00a7800f26b1ab8b79b0b
Yonnys Quintero <leafar.qr@gmail.com>
2023-11-06 17:18:04 -0400

    change structure command

--- app/Console/Commands/SAPIntegration.php
+++ app/Console/Commands/SAPIntegration.php
@@ -3,83 +3,76 @@
 namespace App\Console\Commands;
 
 use App\Models\Customer;
+use App\Models\Product;
+use App\Models\Order;
 use Illuminate\Console\Command;
 use App\Services\SAP\SAPService;
-use Illuminate\Support\Facades\Schema;
 
 class SAPIntegration extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
-    protected $signature = 'sap:get-orders';
+    // ...
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
-    protected $description = 'Command description';
-
-    /**
-     * Execute the console command.
-     */
+    protected $signature = 'sap:sync {type}';
+    protected $description = 'Synchronize SAP data';
 
     public function handle(SAPService $sapService)
     {
-        $this->info('Testing SAP integration to get business partners...');
+        $type = $this->argument('type');
 
+        switch ($type) {
+            case 'customers':
+                $this->syncCustomers($sapService);
+                break;
+            case 'products':
+                $this->syncProducts($sapService);
+                break;
+            case 'orders':
+                $this->syncOrders($sapService);
+                break;
+            // Agrega más casos según sea necesario para otros tipos de datos
+            default:
+                $this->error('Invalid data type specified.');
+                break;
+        }
+
+        $this->info('Synchronization completed.');
+    }
+
+    private function syncCustomers(SAPService $sapService)
+    {
+        $this->info('Syncing customers...');
         $fields = ['CardCode', 'CardName', 'Address', 'FederalTaxID', 'EmailAddress', 'CreditLimit', 'MaxCommitment'];
-
-        // Tamaño del lote
         $batchSize = 20;
-
-        // Iniciar desde el primer registro
         $skip = 0;
-        // Customer::truncate();
-        do {
-            // Obtener los registros de la página actual
-            $response = $sapService->getBusinessPartners($fields, $skip);
 
-            // Verificar si hay registros en la respuesta
+        do {
+            $response = $sapService->getBusinessPartners($fields, $skip);
             if (!empty($response['value'])) {
-                // Procesar los registros del lote actual
-                $this->processBatch($response['value'], $fields);
-                
-                // Actualizar el valor de $skip para la próxima iteración
+                foreach ($response['value']as $record) {
+                    $dataToInsert = array_intersect_key($record, array_flip($fields));
+                    if (is_array($record)) {
+                        if (isset($record['CardCode']) && isset($record['CardName'])){
+                            Customer::updateOrCreate(
+                                ['CardCode' => $record['CardCode']],
+                                $dataToInsert
+                            );
+                        }
+                    }
+                }
                 $skip += $batchSize;
             } else {
-                // Salir del bucle si no hay más registros
                 break;
             }
-
         } while (isset($response['odata.nextLink']));
-
-        $this->info('Test completed.');
     }
 
-    // Método para procesar un lote y almacenarlo en la base de datos
-    private function processBatch($batch, $fields)
+    private function syncProducts(SAPService $sapService)
     {
-        // Puedes almacenar los registros en la base de datos aquí
-        // Por ejemplo, puedes utilizar Eloquent para crear modelos y almacenarlos en la base de datos
-        foreach ($batch as $record) {
-            // Verificar si $record es un array antes de acceder a sus índices
-            echo "Procesando registro {$record['CardCode']}...\n";
-            $dataToInsert = array_intersect_key($record, array_flip($fields));
-            if (is_array($record)) {
-                if (isset($record['CardCode']) && isset($record['CardName'])){
-                    Customer::updateOrCreate(
-                        ['CardCode' => $record['CardCode']],
-                        $dataToInsert
-                    );
-                }
-            }
-        }
+        // Lógica similar para sincronizar productos
     }
 
-
-
+    private function syncOrders(SAPService $sapService)
+    {
+        // Lógica similar para sincronizar órdenes
+    }
 }
