3d4493ab5090072809ae04be31d8f87c55bd28ab
Yonnys Quintero <leafar.qr@gmail.com>
2023-11-07 20:31:44 -0400

    update migrations

--- database/migrations/2023_08_23_225713_create_orders_table.php
+++ database/migrations/2023_08_23_225713_create_orders_table.php
@@ -13,7 +13,6 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
-            $table->unsignedBigInteger('process_id')->nullable();
             $table->unsignedBigInteger('customer_id');
             $table->unsignedBigInteger('order_status_id')->defoult(1);
             $table->integer('DocEntry')->unique();
