e19956b0f9fb55d7e80d0459e091ac7380655a68
Yonnys Quintero <leafar.qr@gmail.com>
2023-11-08 16:25:10 -0400

    add log dashboard

--- app/Console/Commands/SAPIntegration.php
+++ app/Console/Commands/SAPIntegration.php
@@ -7,21 +7,19 @@ use App\Models\Product;
 use App\Models\Customer;
 use App\Models\Warehouse;
 use App\Models\SalesPerson;
+use App\Services\SAP\SyncService;
 use Illuminate\Console\Command;
-use App\Services\SAP\SAPService;
 
 class SAPIntegration extends Command
 {
-
     protected $signature = 'sap:sync {type}';
     protected $description = 'Synchronize SAP data';
-    protected $sapService ;
-    private $batchSize = 20;
+    protected $syncService;
 
-    public function __construct(SAPService $sapService)
+    public function __construct(SyncService $syncService)
     {
         parent::__construct();
-        $this->sapService = $sapService;
+        $this->syncService = $syncService;
     }
 
     public function handle()
@@ -30,19 +28,19 @@ class SAPIntegration extends Command
 
         switch ($type) {
             case 'customers':
-                $this->syncData('business_partners.get', Customer::class);
+                $this->syncService->syncData('business_partners.get', Customer::class);
                 break;
             case 'salesPersons':
-                $this->syncData('sales_persons.get', SalesPerson::class);
+                $this->syncService->syncData('sales_persons.get', SalesPerson::class);
                 break;
             case 'warehouses':
-                $this->syncData('warehouses.get', Warehouse::class);
+                $this->syncService->syncData('warehouses.get', Warehouse::class);
                 break;
             case 'products':
-                $this->syncData('products.get', Product::class);
+                $this->syncService->syncData('products.get', Product::class);
                 break;
             case 'orders':
-                $this->syncOrders();
+                $this->syncService->syncOrders();
                 break;
             default:
                 $this->error('Invalid data type specified.');
@@ -50,69 +48,5 @@ class SAPIntegration extends Command
         }
 
         $this->info('Synchronization completed.');
-    }
-
-
-    private function syncData(string $endpoint, $modelClass)
-    {
-        $this->info("Syncing $endpoint...");
-
-        $skip = 0;
-
-        do {
-            $fields = $modelClass::FILLABLE;
-            $identifier = $modelClass::IDENTIFIER;
-
-            $lastSyncedOrder = $modelClass::latest('docnum')->first();
-
-            $filterParam = $lastSyncedOrder ? "DocNum ge {$lastSyncedOrder->DocNum}" : null;
-    
-
-            $response = $this->sapService->get($endpoint, $skip, $fields, $filterParam);
-            
-            if (!empty($response['value'])) {
-                foreach ($response['value'] as $record) {
-                    $dataToInsert = array_intersect_key($record, array_flip($fields));
-                    
-                    if (is_array($record) && isset($record[$identifier])) {
-                        $modelClass::updateOrCreate(
-                            [ $identifier => $record[$identifier] ],
-                            $dataToInsert
-                        );
-                    }
-                }
-                $skip += $this->batchSize;
-            } else {
-                break;
-            }
-        } while (isset($response['odata.nextLink']));
-    }
-
-
-    private function syncOrders()
-    {
-        $endpoint = 'orders.get'; // Ajusta el nombre del endpoint según tu estructura
-        $modelClass = Order::class;
-        
-        $this->info("Syncing $endpoint...");
-
-        $skip = 0;
-
-        do {
-            $fields = $modelClass::FILLABLE_API;
-            $identifier = $modelClass::IDENTIFIER;
-
-            $response = $this->sapService->get($endpoint, $skip, $fields);
-
-            if (!empty($response['value'])) {
-                foreach ($response['value'] as $orderData) {
-
-                    $modelClass::syncOrderWithItems($orderData);
-                }
-                $skip += $this->batchSize;
-            } else {
-                break;
-            }
-        } while (isset($response['odata.nextLink']));
     }
 }
---
+++ app/Services/SAP/SyncService.php
@@ -0,0 +1,92 @@
+<?php
+namespace App\Services\SAP;
+
+use App\Models\Order;
+use App\Services\SAP\SAPService;
+use Illuminate\Support\Facades\Log;
+
+class SyncService
+{
+    protected $sapService;
+    private $batchSize = 20;
+
+    public function __construct(SAPService $sapService)
+    {
+        $this->sapService = $sapService;
+    }
+
+    public function syncData(string $endpoint, $modelClass)
+    {
+        $skip = 0;
+
+        try {
+            do {
+                $fields = $modelClass::FILLABLE;
+                $identifier = $modelClass::IDENTIFIER;
+                $response = $this->sapService->get($endpoint, $skip, $fields, $filterParam);
+
+                if (!empty($response['value'])) {
+                    foreach ($response['value'] as $record) {
+                        $dataToInsert = array_intersect_key($record, array_flip($fields));
+
+                        if (is_array($record) && isset($record[$identifier])) {
+                            $modelClass::updateOrCreate(
+                                [$identifier => $record[$identifier]],
+                                $dataToInsert
+                            );
+                        }
+                    }
+                    $skip += $this->batchSize;
+                } else {
+                    break;
+                }
+            } while (isset($response['odata.nextLink']));
+        } catch (\Exception $e) {
+
+            $this->logError($e, $modelClass);
+
+        }
+    }
+
+    public function syncOrders()
+    {
+        $endpoint = 'orders.get';
+        $modelClass = Order::class;
+
+        $skip = 0;
+
+        try {
+            do {
+                $fields = $modelClass::FILLABLE_API;
+                $identifier = $modelClass::IDENTIFIER;
+
+                $lastSyncedOrder = $modelClass::latest('DocNum')->first();
+                $filterParam = $lastSyncedOrder ? "DocNum ge {$lastSyncedOrder->DocNum}" : null;
+
+                $response = $this->sapService->get($endpoint, $skip, $fields, $filterParam);
+
+                if (!empty($response['value'])) {
+                    foreach ($response['value'] as $orderData) {
+                        $this->log("Loading orders {$orderData['DocNum']}...");
+                        $modelClass::syncOrderWithItems($orderData);
+                    }
+                    $skip += $this->batchSize;
+                } else {
+                    break;
+                }
+            } while (isset($response['odata.nextLink']));
+        } catch (\Exception $e) {
+            $this->logError($e, $modelClass, $lastSyncedOrder->DocNum);
+        }
+    }
+
+    private function logError(\Exception $exception, $modelClass, $lastSyncedOrder = null)
+    {
+        Log::error("Error syncing {$modelClass} - DocNum: {$lastSyncedOrder->DocNum}. Error: {$exception->getMessage()}");
+    }
+
+    private function log($message)
+    {
+        echo $message . PHP_EOL;
+    }
+}
