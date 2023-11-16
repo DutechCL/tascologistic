b85573b842edf08fce0e86ad4c923b8cfc284f37
Yonnys Quintero <leafar.qr@gmail.com>
2023-11-16 10:29:01 -0400

    change method

--- app/Services/SAP/SyncService.php
+++ app/Services/SAP/SyncService.php
@@ -17,79 +17,70 @@ class SyncService
 
     public function sync(array $config)
     {
-
-        // dd($config);
         $skip = 0;
-        $createdOrUpdatedCount = 0;
+        $count = 0;
         try {
             
             extract($config); // $endpoint, $model, $fields, $identifier, $method, $filter
 
-            // $lastSyncedRecord = $model::latest('CreateDate')->first();
-
-            // if($lastSyncedRecord){
-            //     $filterParam = $lastSyncedRecord->CreateDate ? "CreateDate ge $lastSyncedRecord->CreateDate and CreateTime gt $lastSyncedRecord->CreateTime" : null;
-            // }
-
-            // Inicializamos la URL con el operador lógico principal
-
-            $url = '';
-
-            // Iteramos sobre los parámetros
-            foreach ($filter['params'] as $param) {
-                // Agregamos el operador lógico entre parámetros
-                if (!empty($url)) {
-                    $url .= ' ' . strtoupper($filter['operator']) . ' ';
-                }
-
-                // Agregamos el campo, operador y valor al URL
-                $url .= "{$param['field']} {$param['operator']} {$param['value']}";
-            }
-
-            dd($url, $filter);
+            $params = $this->buildUrlParams($filter);
 
 
             do {
-                $response = $this->sapService->get($endpoint, $skip, $fields);
-    
+                $response = $this->sapService->get($endpoint, $skip, $fields, $params);
+                dd($response);
                 if (!empty($response['value'])) {
-                    try {
-
-                        $argsArray = array_map(function ($record) use ($model, $method, $identifier, $fields) {
-
-                            $dataToInsert = array_intersect_key($record, array_flip($fields));
-                
-                            if (is_array($record) && isset($record[$identifier])) {
-
-                                call_user_func(
-                                    [$model, $method], // model and method to call
-                                    [$identifier => $record[$identifier]],  // params #1
-                                    $dataToInsert // params #2
-                                );
-
-                            }
-                
-                            return null;
-                        }, $response['value']);
-                
-                        $createdOrUpdatedCount += count($argsArray);
-                
-                    } catch (\Exception $e) {
-                        $this->logError($e, $model);
-                    }
+                    $count += $this->createRecords($model, $method, $identifier, $fields, $response);
                 }
     
                 $skip += $this->batchSize;
     
             } while (isset($response['odata.nextLink']));
     
-            return $createdOrUpdatedCount;
+            return $count;
     
         } catch (\Exception $e) {
             $this->logError($e, $modelClass);
 
             return -1;
         }
+    }
+
+    public function buildUrlParams(array $filter)
+    {
+        $url = '';
+
+        foreach ($filter['params'] as $param) {
+            // Agregamos el operador lógico entre parámetros
+            if (!empty($url)) {
+                $url .= ' ' . $param['operator'] . ' ';
+            }
+
+            $url .= "{$param['field']} {$param['operator']} {$param['value']}";
+        }
+
+        return $url;
+    }
+
+    public function createRecord($modelClass, $method, $identifier, $fields, $response)
+    {
+        $count = 0;
+        
+        try {
+            foreach ($response['value'] as $record) {
+                $dataToInsert = array_intersect_key($record, array_flip($fields));
+
+                if (is_array($record) && isset($record[$identifier])) {
+
+                    $model::$method([$identifier => $record[$identifier]], $dataToInsert);
+                    $count++;
+
+                }
+            }
+        } catch (\Exception $e) {
+            $this->logError($e, $model);
+        }
+        return $count;
     }
 
     private function logError(\Exception $exception, $modelClass, $lastSyncedOrder = null)
