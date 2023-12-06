f2733ada68d31f15b4263e657c8a451ffb9ba18a
Yonnys Quintero <leafar.qr@gmail.com>
2023-12-06 15:48:04 -0400

    Add missing seeders and uncomment commented seeders

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,23 +14,25 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(RolesTableSeeder::class);
         $this->call(ModelHasRolesTableSeeder::class);
-        // $this->call(CustomersTableSeeder::class);
+        $this->call(CustomersTableSeeder::class);
         $this->call(SalesPersonsTableSeeder::class);
         $this->call(WarehousesTableSeeder::class);
-        // $this->call(ProductsTableSeeder::class);
+        $this->call(ProductsTableSeeder::class);
         $this->call(ProblemsTableSeeder::class);
         $this->call(MethodShippingsTableSeeder::class);
         $this->call(PaymentMethodsTableSeeder::class);
         $this->call(OrderStatusesTableSeeder::class);
         $this->call(ProcessesTableSeeder::class);
-        // $this->call(OrdersTableSeeder::class);
-        // $this->call(OrderItemsTableSeeder::class);
+        $this->call(OrdersTableSeeder::class);
+        $this->call(OrderItemsTableSeeder::class);
         $this->call(PermissionsTableSeeder::class);
         $this->call(UsersTableSeeder::class);
         $this->call(RoleHasPermissionsTableSeeder::class);
         $this->call(MenuItemsTableSeeder::class);
         $this->call(SettingsTableSeeder::class);
         $this->call(UserWarehouseTableSeeder::class);
-        // $this->call(LogOrdersTableSeeder::class);
+        $this->call(LogOrdersTableSeeder::class);
+        $this->call(CustomerAddressesTableSeeder::class);
+        $this->call(CustomerContactEmployeesTableSeeder::class);
     }
 }
