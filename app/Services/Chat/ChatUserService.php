6caf4200d3041faa898d68037dfdf8b81d08e3ec
Yonnys Quintero <leafar.qr@gmail.com>
2023-12-18 16:54:15 -0400

    Fix pagination issue in listChatsByUser method

--- app/Services/Chat/ChatUserService.php
+++ app/Services/Chat/ChatUserService.php
@@ -57,7 +57,7 @@ class ChatUserService
             
             $chats = $query->where('status', Chat::STATUS_OPEN) ->limit(20)->get();
 
-            $chats = $chats->data->map(function ($chat) {
+            $chats = $chats->map(function ($chat) {
                 return [
                     'chat' => $chat,   // Puedes ajustar esto según tu necesidad
                     'order' => new OrderResource($chat->order),
